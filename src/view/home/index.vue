4a947abca59ed423dfb59b6fd583d6d7d57b7670
moxccc <1355926601@qq.com>
2023-12-14 14:11:34 +0800

    1

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,52 +1,13 @@
 <template>
     <div>
-        <button @click="fk">发送信息</button>
+        <button @click="fk">啦啦啦我复活啦</button>
     </div>
 </template>
 
 <script setup>
-import msgpack from 'msgpack-lite';
-
-import { ref } from 'vue'
-
-const ws = ref("");
-
-ws.value = new WebSocket(`ws://192.168.1.148:5398/Chat?name=小王&email=11`);
-ws.value.onmessage = async function (event) {
-    try {
-        let data;
-        if (event.data instanceof Blob) {
-            // 异步地将 Blob 转换为 ArrayBuffer
-            const arrayBuffer = await blobToArrayBuffer(event.data);
-            data = msgpack.decode(new Uint8Array(arrayBuffer));
-        } else {
-            // 如果 event.data 已经是 ArrayBuffer 或 Uint8Array
-            data = msgpack.decode(new Uint8Array(event.data));
-        }
-        console.log(data); // 解码后的数据
-    } catch (error) {
-        console.error("解码错误", error);
-    }
-}
 const fk=()=>{
-    let Lee=[1,"sg",""];
-    var buffer = msgpack.encode(Lee);
-    ws.value.send(buffer);
+    alert("run")
 }
-
-// 定义一个将 Blob 转换为 ArrayBuffer 的异步函数
-function blobToArrayBuffer(blob) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsArrayBuffer(blob);
-    });
-}
-
-
-
-
 </script>
 
 <style lang="scss" scoped></style>
